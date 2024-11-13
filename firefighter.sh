6d99d8de574e94c05713f1062f0ad4f230a023b7
Conrad <20105131+shaives@users.noreply.github.com>
2024-11-13 11:36:03 -0800

    created shell file

---
+++ firefighter.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH --array=0-4
+#SBATCH --job-name globalMeltdown
+#SBATCH --nodes=1 --tasks-per-node=2 --mem=16GB
+#SBATCH --output=%j_%a_log.txt
+#SBATCH --time 0-24:00:00
+#SBATCH --mail-user conrad.urban.gy@nps.edu
+#SBATCH --mail-type END 
+
+. /etc/profile
+source /smallwork/$USER/comp3/bin/activate
+
+python meltdown.py
